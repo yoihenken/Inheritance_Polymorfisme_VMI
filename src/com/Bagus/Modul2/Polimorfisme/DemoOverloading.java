f2ec991d4cc4cfb5655eb5ada5eef8c87f7a7022
Bagus Bayu Sasongko <18104005@ittelkom-pwt.ac.id>
2020-10-13 10:16:57 +0700

    Add Overloading (Polimorphism)

---
+++ src/com/Bagus/Modul2/Polimorfisme/DemoOverloading.java
@@ -0,0 +1,11 @@
+package com.Bagus.Modul2.Polimorfisme;
+
+public class DemoOverloading {
+    public static void main(String[] args) {
+        Lagu lagu1 = new Lagu("Yoru ni Kakeru");
+        Lagu lagu2 = new Lagu("Halu", "Feby");
+
+        lagu1.getDataLagu();
+        lagu2.getDataLagu();
+    }
+}
---
+++ src/com/Bagus/Modul2/Polimorfisme/Lagu.java
@@ -0,0 +1,23 @@
+package com.Bagus.Modul2.Polimorfisme;
+
+public class Lagu {
+    public String judul;
+    public String pencipta;
+
+    //Overloading
+    public Lagu(String judul) {
+        this.judul = judul;
+    }
+
+    //Overloading
+    public Lagu(String judul, String pencipta) {
+        this.judul = judul;
+        this.pencipta = pencipta;
+    }
+
+    public void getDataLagu(){
+        System.out.println("Judul\t\t: " + judul);
+        System.out.println("Pencipta\t: " + pencipta);
+        System.out.println();
+    }
+}
